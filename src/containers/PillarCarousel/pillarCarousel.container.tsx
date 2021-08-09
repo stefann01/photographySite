8a83c36468fe4459357eac652adab88806f49fd9
Stefan Mares <stefanutmares@gmail.com>
2021-08-09 21:37:58 +0300

    Some renamings

--- src/containers/PillarCarousel/pillarCarousel.container.tsx
+++ src/containers/PillarCarousel/pillarCarousel.container.tsx
@@ -1,9 +1,9 @@
 import PillarCarousel from "../../components/PillarCarousel/pillarCarousel";
-import img1 from "../../assets/_MG_3954-min.jpg";
-import img2 from "../../assets/_MG_3821-min.jpg";
-import img3 from "../../assets/_MG_3815-min.jpg";
-import img4 from "../../assets/_MG_3964-min.jpg";
-import img5 from "../../assets/_MG_3847-min.jpg";
+import img1 from "../../assets/images/img1.jpg";
+import img2 from "../../assets/images/img2.jpg";
+import img3 from "../../assets/images/img3.jpg";
+import img4 from "../../assets/images/img4.jpg";
+import img5 from "../../assets/images/img5.jpg";
 import "./pillarCarousel.container.scss";
 import SectionDescription from "../../components/SectionDescription/sectionDescription";
 
